ceeca42ef745272b63f6988c88a2f5b7503bd229
danny-comex <danny_6868358@hotmail.com>
2018-11-15 08:47:44 -0500

    models

--- apps/flujo/models.py
+++ apps/flujo/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+from apps.flujo import properties
+
+
+class Moneda(models.Model):
+    pais = models.CharField(max_length=200)
+    nombre = models.CharField(max_length=200)
+
+#, blank=True, null=True, default=""
+class ActivoCaja(models.Model):
+    nombre_Activo = models.CharField(max_length=255)
+    valor_activo = models.DecimalField(decimal_places=2)
+    moneda = models.ForeignKey(Moneda, on_delete=models.CASCADE())
+    tiempo = models.CharField(max_length=30, choices=properties.TIPO_TIEMPO)
+    valor_tiempo = models.PositiveIntegerField()
+
+
+class Proveedor(models.Model):
+    nombre_proveedor = models.CharField(max_length=100)
+
+class Obligaciones(models.Model):
+    nombre = models.ForeignKey(Proveedor, on_delete=models.CASCADE)
+    falta_pagar_dinero = models.DecimalField(decimal_places=2)
+    falta_pagar_dias = models.CharField(max_length=30, choices=properties.TIPO_TIEMPO)
+    descripcion = models.CharField(max_length=250)
+    tasa = models.DecimalField(decimal_places=2)
+
+
---
+++ apps/flujo/properties.py
@@ -0,0 +1,12 @@
+DIA="DIA"
+MES = "MES"
+HORA ="HORA"
+
+
+TIPO_TIEMPO =(
+    (HORA,"Hora")
+    (DIA, "Dia")
+    (HORA, "Hora")
+
+
+)
